ac0a99c5a1679f80237d8d3d3b475362fbfd67d8
Darren Cauthon <darren@cauthon.com>
2015-01-04 10:40:06 -0600

    Refactor.

--- sample.cmproj/run.rb
+++ sample.cmproj/run.rb
@@ -10,14 +10,7 @@ class GapFiller
     File.open('project.xml').read
   end
 
-  def find_gaps_in track_id
-
-    doc = Nokogiri::XML xml
-
-    tracks = doc.xpath("//GenericTrack")
-
-    track = tracks.select { |t| t['id'] == track_id }.first
-
+  def find_items_in track
     items = track.xpath('./Medias/ScreenVMFile').map do |item|
               { 
                 id:       item['id'],
@@ -29,23 +22,42 @@ class GapFiller
 
     items.each { |i| i[:next_item_start] = i[:start] + i[:duration] }
 
-    gaps = items.each_with_index.select do |item, index|
-             result = false
-             if index > 0 && index < items.count
-               last_item = items[index - 1]
-               result = item[:start] != last_item[:next_item_start]
-             end
-             result
-           end.map do |item, index|
-             last_item = items[index - 1]
-             {
-               index:      index - 1,
-               start:      last_item[:next_item_start],
-               id:         last_item[:id],
-               duration:   last_item[:duration],
-               gap_length: item[:start] - last_item[:next_item_start],
-             }
-           end
+    items
+  end
+
+  def find_track track_id
+    doc = Nokogiri::XML xml
+    tracks = doc.xpath("//GenericTrack")
+    tracks.select { |t| t['id'] == track_id }.first
+  end
+
+  def find_the_gaps_in track
+    items = find_items_in track
+
+    items.each_with_index.select do |item, index|
+      result = false
+      if index > 0 && index < items.count
+        last_item = items[index - 1]
+        result = item[:start] != last_item[:next_item_start]
+      end
+      result
+    end.map do |item, index|
+      last_item = items[index - 1]
+      {
+        index:      index - 1,
+        start:      last_item[:next_item_start],
+        id:         last_item[:id],
+        duration:   last_item[:duration],
+        gap_length: item[:start] - last_item[:next_item_start],
+      }
+    end
+  end
+
+  def find_the_copies_in track_id
+
+    track = find_track track_id
+
+    gaps = find_the_gaps_in track
 
     copies_to_make = gaps.map do |gap|
                        (0..(gap[:gap_length]/gap[:duration])).to_a.map do |index|
@@ -69,8 +81,10 @@ class GapFiller
       copy.delete :id
     end
 
+    copies_to_make
+
   end
 
 end
 
-puts GapFiller.new('project.xml').find_gaps_in('10')
+puts GapFiller.new('project.xml').find_the_copies_in('10')
